dbd24ecdd8f09e488704ce7fb94d75c3950fea0e
Chris Zachary <chris@pivvot.co>
2019-01-05 13:36:12 -0600

    fix issue with expressions in cc2018+ attempt 1

--- renderer/tasks/patch.js
+++ renderer/tasks/patch.js
@@ -1,11 +1,15 @@
-'use strict';
+"use strict";
 
-const path      = require('path');
-const fs        = require('fs-extra');
-const async     = require('async');
+const path = require("path");
+const fs = require("fs-extra");
+const async = require("async");
+
+var DOMParser = require("xmldom").DOMParser;
+var TheSerializer = require("xmldom").XMLSerializer;
+var fulldom = require("xmldom");
 
 function getAllExpressions(data) {
-    return data.match(/\<expr bdata=\"([a-f0-9]+)\"\s*\/\>/gi);
+  return data.match(/\<expr bdata=\"([a-f0-9]+)\"\s*\/\>/gi);
 }
 
 /**
@@ -15,56 +19,92 @@ function getAllExpressions(data) {
  *     "/Users/Name/Projects/MyProject/"
  *     "C:\\Projects\\MyNewProject\\"
  *     "/usr/var/tmp/projects/123/"
- * 
+ *
  * And will replace them to string `dst`
  */
 function replacePath(src, dst) {
-    return src.replace( /(?:(?:[A-Z]\:|~){0,1}(?:\/|\\\\|\\)(?=[^\s\/]))(?:(?:[\ a-zA-Z0-9\+\-\_\.\$\●\-]+(?:\/|\\\\|\\)))*/gm, dst);
+  return src.replace(
+    /(?:(?:[A-Z]\:|~){0,1}(?:\/|\\\\|\\)(?=[^\s\/]))(?:(?:[\ a-zA-Z0-9\+\-\_\.\$\●\-]+(?:\/|\\\\|\\)))*/gm,
+    dst
+  );
 }
 
 function processTemplateFile(project, callback) {
-    // project file template name
-    let projectName     = path.join( project.workpath, project.template );
-    let replaceToPath   = path.join( process.cwd(), project.workpath, path.sep); // absolute path
+  // project file template name
+  let projectName = path.join(project.workpath, project.template);
+  let replaceToPath = path.join(process.cwd(), project.workpath, path.sep); // absolute path
 
-    // escape single backslash to double in win
-    replaceToPath = replaceToPath.replace(/\\/g, '\\\\');
+  // escape single backslash to double in win
+  replaceToPath = replaceToPath.replace(/\\/g, "\\\\");
 
-    // read project file contents
-    fs.readFile(projectName, (err, bin) => {
-        if (err) return callback(err);
+  // read project file contents
+  // FIX expressions issue
+  // fs.readFile(projectName, (err, bin) => {
+  //     if (err) return callback(err);
 
-        // convert to utf8 string
-        let data = bin.toString('utf8');
+  //     // convert to utf8 string
+  //     let data = bin.toString('utf8');
 
-        // check for valid project template
-        if (data.indexOf('<?xml') !== 0) return callback(new Error('Project is not valid xml project template'));
+  //     // check for valid project template
+  //     if (data.indexOf('<?xml') !== 0) return callback(new Error('Project is not valid xml project template'));
 
-        // search for expressions
-        let expressions = getAllExpressions(data);
+  //     // search for expressions
+  //     let expressions = getAllExpressions(data);
 
-        // check for existing expressions
-        if (expressions !== null) {
+  //     // check for existing expressions
+  //     if (expressions !== null) {
 
-            // then iterate over them
-            for (let expr of expressions) {
+  //         // then iterate over them
+  //         for (let expr of expressions) {
 
-                // extract hex from xml tag and decode it
-                let hex = expr.split('"')[1];
-                let dec = new Buffer(hex, 'hex').toString('utf8');
-    
-                // do patch and encode back to hex
-                // using regex file path pattern
-                let enc = new Buffer( replacePath( dec, replaceToPath ) ).toString('hex');
-    
-                // replace patched hex
-                data = data.replace( hex, enc );
-            }
+  //             // extract hex from xml tag and decode it
+  //             let hex = expr.split('"')[1];
+  //             let dec = new Buffer(hex, 'hex').toString('utf8');
+
+  //             // do patch and encode back to hex
+  //             // using regex file path pattern
+  //             let enc = new Buffer( replacePath( dec, replaceToPath ) ).toString('hex');
+
+  //             // replace patched hex
+  //             data = data.replace( hex, enc );
+  //         }
+  //     }
+
+  //     // save result
+  //     fs.writeFile(projectName, data, callback);
+  // });
+  fs.readFile(projectName, (err, bin) => {
+    if (err) return callback(err);
+
+    // convert to utf8 string
+    let data = bin.toString("utf8");
+    // create xml
+    var xmlDoc = new DOMParser().parseFromString(data);
+    // search all scripts
+    var stringElements = xmlDoc.getElementsByTagName("string");
+    // process in xml way
+
+    for (var key = 0; key < stringElements.length; key++) {
+      var elm = stringElements[key];
+      var original = elm.textContent;
+      if (original.indexOf("//nex") != -1) {
+        elm.textContent = replacePath(
+          original.replace("//nex", ""),
+          replaceToPath
+        );
+        if (elm.textContent != original) {
+          console.log("#----");
+          console.log("original: ", elm.textContent, original);
+          console.log("changed to:", elm.textContent);
         }
-        
-        // save result
-        fs.writeFile(projectName, data, callback);
-    });
+      }
+    }
+
+    data = new TheSerializer().serializeToString(xmlDoc);
+
+    // save result
+    fs.writeFile(projectName, data, callback);
+  });
 }
 
 /**
@@ -73,21 +113,20 @@ function processTemplateFile(project, callback) {
  * to ones that provided in project
  */
 module.exports = function(project) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    console.info(`[${project.uid}] patching project...`);
 
-        console.info(`[${project.uid}] patching project...`);
+    // Iterate over assets,
+    // skip those that are not data/script files,
+    for (let asset of project.assets) {
+      if (["script", "data"].indexOf(asset.type) === -1) continue;
 
-        // Iterate over assets, 
-        // skip those that are not data/script files, 
-        for (let asset of project.assets) {
-            if (['script', 'data'].indexOf(asset.type) === -1) continue;
+      return processTemplateFile(project, err => {
+        return err ? reject(err) : resolve(project);
+      });
+    }
 
-            return processTemplateFile(project, (err) => {
-                return (err) ? reject(err) : resolve(project);
-            });
-        }
-
-        // project contains no data/script assets, pass
-        resolve(project);
-    });
+    // project contains no data/script assets, pass
+    resolve(project);
+  });
 };
